c7bca49593a563f0aaf2081abd3d18860d21086f
Victor Hernandez <vik407@gmail.com>
2020-05-16 20:32:04 +0000

    2. Select some Amenities to be comfortable! 0x0

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,18 @@
 $(document).ready(() => {
   const amenCheckList = {}
-  $('input[type=checkbox]').change(function() {
-    if ($(this).prop('checked') === true) {
+  $('input[type=checkbox]').change(function () {
+    if ($(this).is(':checked')) {
       let key = $(this).attr('data-id')
       let value = $(this).attr('data-name')
       amenCheckList[key] = value
-    } else if ($(this).prop('checked') === false) {
+    }
+    if (!$(this).is(':checked')) {
       delete amenCheckList[$(this).attr('data-id')]
     }
     const list = [];
     for (const name in amenCheckList) {
       list.push(amenCheckList[name]);
     }
-    $('DIV.amenities H4').text(list.join(', '));
+    $('div.amenities h4').text(list.join(', '));
   });
 });
